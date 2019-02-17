274148c5f766dddede5ae01a79c17dc1cf88e75b
Mickael Sergent <mickael.sergent@free.fr>
2019-02-17 12:51:58 +0100

    add test with modification of key/text len

--- test/aes_tst_main.c
+++ test/aes_tst_main.c
@@ -24,8 +24,8 @@ CTEST(aes_test, test_aes_simple_192_txt_len)
 {
   unsigned char sTextACrypter[] = "ClaudiusCaligulaAUGUSTUS";
   unsigned char sClef[]         = "123456789ABCDEF0";
-  unsigned char sTextCrypter[17];;
-  unsigned char sResultatApresDecryptage[17];
+  unsigned char sTextCrypter[25];
+  unsigned char sResultatApresDecryptage[25];
   int result;
 
   result = aes_cipher(sTextACrypter, sTextCrypter, sClef, 192, 128);
@@ -36,6 +36,60 @@ CTEST(aes_test, test_aes_simple_192_txt_len)
 
   ASSERT_DATA(sResultatApresDecryptage, 24, sTextACrypter, 24);
 }
+
+CTEST(aes_test, test_aes_simple_256_txt_len)
+{
+  unsigned char sTextACrypter[] = "Sed ut perspiciatis unde omnis i";
+  unsigned char sClef[]         = "123456789ABCDEF0";
+  unsigned char sTextCrypter[33];
+  unsigned char sResultatApresDecryptage[33];
+  int result;
+
+  result = aes_cipher(sTextACrypter, sTextCrypter, sClef, 256, 128);
+  ASSERT_EQUAL(result, AES_OK);
+
+  result = aes_uncipher(sTextCrypter, sResultatApresDecryptage, sClef, 256, 128);
+  ASSERT_EQUAL(result, AES_OK);
+
+  ASSERT_DATA(sResultatApresDecryptage, 32, sTextACrypter, 32);
+}
+
+CTEST(aes_test, test_aes_simple_192_key_len)
+{
+  unsigned char sTextACrypter[] = "Sed ut perspiciatis unde omnis i";
+  unsigned char sClef[]         = "123456789ABCDEF012345678";
+  unsigned char sTextCrypter[33];
+  unsigned char sResultatApresDecryptage[33];
+  int result;
+
+  result = aes_cipher(sTextACrypter, sTextCrypter, sClef, 256, 192);
+  ASSERT_EQUAL(result, AES_OK);
+
+  result = aes_uncipher(sTextCrypter, sResultatApresDecryptage, sClef, 256, 192);
+  ASSERT_EQUAL(result, AES_OK);
+
+  ASSERT_DATA(sResultatApresDecryptage, 32, sTextACrypter, 32);
+}
+
+
+
+CTEST(aes_test, test_aes_simple_256_key_len)
+{
+  unsigned char sTextACrypter[] = "Sed ut perspiciatis unde omnis i";
+  unsigned char sClef[]         = "123456789ABCDEF0123456789AbCdEf0";
+  unsigned char sTextCrypter[33];
+  unsigned char sResultatApresDecryptage[33];
+  int result;
+
+  result = aes_cipher(sTextACrypter, sTextCrypter, sClef, 256, 256);
+  ASSERT_EQUAL(result, AES_OK);
+
+  result = aes_uncipher(sTextCrypter, sResultatApresDecryptage, sClef, 256, 256);
+  ASSERT_EQUAL(result, AES_OK);
+
+  ASSERT_DATA(sResultatApresDecryptage, 32, sTextACrypter, 32);
+}
+
 
 
 int main(int argc, const char* argv[])
